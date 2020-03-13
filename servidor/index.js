a449c2cfbdc1463ce29720d0a2b7a2a41ad53d61
tekilla501 <chinovyou@gmail.com>
2020-03-13 12:42:45 -0600

    cuarta insercion

---
+++ servidor/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+//Crear el servidor
+const app = express();
+
+//Puerto de la app
+const PORT = process.env.PORT || 3000;
+
+//Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`)
+}); 
